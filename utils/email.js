58b673edd737b370a073047350b7300153cf4090
Ryan Esmores <ryanesmores07@gmail.com>
2023-12-08 00:52:54 +0900

    Building a complex email handler

--- utils/email.js
+++ utils/email.js
@@ -1,9 +1,20 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  try {
-    // 1) Create a transporter
-    const transporter = nodemailer.createTransport({
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `admin <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+    return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
       auth: {
@@ -13,21 +24,34 @@ const sendEmail = async (options) => {
       secure: false, // This is important for STARTTLS
       requireTLS: true,
     });
+  }
 
-    // 2) Define the email options
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // 2) Define email options
     const mailOptions = {
-      from: 'admin <admin2@ryan.ios>',
-      to: options.email,
-      subject: options.subject,
-      text: options.message,
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
     };
 
-    // 3) Actually send the email
-    await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.error('Error sending email:', error);
-    throw new Error('Error sending email');
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
   }
 };
-
-module.exports = sendEmail;
