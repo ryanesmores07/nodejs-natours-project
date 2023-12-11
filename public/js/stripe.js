4b72213e7efefffc9ad2e4a89d8c87fa24a27591
Ryan Esmores <ryanesmores07@gmail.com>
2023-12-12 00:08:14 +0900

    removed config.env

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,13 +1,13 @@
-// import axios from 'axios';
+import axios from 'axios';
 // const stripe = Stripe(
 //   'pk_test_51OLhVMLkspFzUJgfTXTIdQOPSAXlz5VA3KQeXM646DIjYQEVEu28PfVHXO6caZMVpNWoDUOOqP6E4NX35GcxBhVr00s0PliJoH'
 // );
 
-// export const bookTour = async (tourId) => {
-//   // 1) Get checkout session from API
-//   const session = await axios(
-//     `http://127.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-//   );
+export const bookTour = async (tourId) => {
+  // 1) Get checkout session from API
+  const session = await axios(
+    `http://127.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+  );
 
-//   // 2) Create checkout form + charge credit card
-// };
+  // 2) Create checkout form + charge credit card
+};
