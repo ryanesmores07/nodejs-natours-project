999b88326c9b3dd7fff0acc832406f8824c1dde9
Ryan Esmores <ryanesmores07@gmail.com>
2023-12-16 23:48:16 +0900

    Created the Booking Model

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
+import { showAlert } from './alert';
 
 export const bookTour = async (tourId) => {
   const stripe = Stripe(
     'pk_test_51OLhVMLkspFzUJgfTXTIdQOPSAXlz5VA3KQeXM646DIjYQEVEu28PfVHXO6caZMVpNWoDUOOqP6E4NX35GcxBhVr00s0PliJoH'
   );
-  // 1) Get checkout session from API
-  const session = await axios(
-    `http://127.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-  );
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
 
-  // 2) Create checkout form + charge credit card
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (error) {
+    showAlert('error', error);
+  }
 };
