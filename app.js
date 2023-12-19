cacf7f90a5d7ac75e2e93c7be783bcdc8dde9b03
Ryan Esmores <ryanesmores07@gmail.com>
2023-12-19 23:59:10 +0900

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -19,11 +20,27 @@ const bookingRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
-app.enable("trust proxy")
+app.enable('trust proxy');
 
 // Set view engine and views directory
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+// Global Middlewares
+// Implement CORSa
+app.use(cors());
+// Access-Control-Allow-Origin *
+
+// api.natours.com, front-end natours.com
+// Only for specific domain
+// app.use(cors({
+// origin: "https://www.natours.com"
+// }))
+
+app.options('*', cors());
+
+// Only allow patch, delete on tours
+// app.options("/api/v1/tours/:id", cors())
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
